cc03c0452c09ab7a2fb0200e7217720678f58866
hung-dt <dth.online@gmail.com>
2026-01-07 02:18:39 +0700

    Chapter 4 completed.

--- microblog.py
+++ microblog.py
@@ -1,1 +1,11 @@
-from app import flask_app
+import sqlalchemy as sa
+import sqlalchemy.orm as so
+
+from app import flask_app, db
+from app.models import User, Post
+
+
+# creates a shell context that adds the database instance and models to the shell session
+@flask_app.shell_context_processor
+def make_shell_context():
+    return {"sa": sa, "so": so, "db": db, "User": User, "Post": Post}
