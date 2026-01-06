613e5b07a14d4c61808eb0f003b67ff435739fb0
hung-dt <dth.online@gmail.com>
2026-01-07 00:16:49 +0700

    Chapter 2 completed.

--- app/routes.py
+++ app/routes.py
@@ -1,7 +1,15 @@
+from flask import render_template
 from app import flask_app
 
 
 @flask_app.route("/")
 @flask_app.route("/index")
 def index():
-    return "Hello World from Flask!"
+    user = {"username": "Hung"}
+    posts = [
+        {"author": {"username": "John"}, "body": "Beautiful day in Portland!"},
+        {"author": {"username": "Susan"}, "body": "The Avengers movie was so cool!"},
+        {"author": {"username": "BSB"}, "body": "Show me the meaning of being lonely!"},
+        {"author": {"username": "Avril Lavigne"}, "body": "My Happy Ending!"},
+    ]
+    return render_template("index.html", title="Home", user=user, posts=posts)
